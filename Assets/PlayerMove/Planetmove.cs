da46168909cb9df5ce045bfb925b0a2b7d9a62d9
MSI\GA2A <aa>
2024-10-09 14:04:10 +0900

    惑星回転追加。 現状；矢印キーで惑星の回転が可能

--- Assets/PlayerMove/Planetmove.cs
+++ Assets/PlayerMove/Planetmove.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 //惑星となる球の動作
 public class Planetmove : MonoBehaviour
 {
-    public float movespeed;//回転するスピード
-    public float msx, msy;//スピードではなく数の変更をする↑
+    public float speed = 1.0f, x_rotation = 0.0f, y_rotation = 0.0f;//スピードではなく数の変更をする↑
 
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;//フレームレートを60に設定
-        movespeed = 0.1f;
+        Application.targetFrameRate = 30;//フレームレートを60に設定
     }
 
     // Update is called once per frame
@@ -27,23 +25,30 @@ public class Planetmove : MonoBehaviour
         //右キーが押された場合
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, movespeed, transform.eulerAngles.z));
-            movespeed += 0.001f;
+            transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, y_rotation, transform.eulerAngles.z));
+            y_rotation += speed;
+            Debug.Log("右キーが押された");
         }
         //左キーが押された場合
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-
+            transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, y_rotation, transform.eulerAngles.z));
+            y_rotation -= speed;
+            Debug.Log("左キーが押された");
         }
         //上キーが押された場合
         if (Input.GetKey(KeyCode.UpArrow))
         {
-
+            transform.rotation = Quaternion.Euler(new Vector3(x_rotation, transform.eulerAngles.y, transform.eulerAngles.z));
+            x_rotation += speed;
+            Debug.Log("上キーが押された");
         }
         //下キーが押された場合
         if (Input.GetKey(KeyCode.DownArrow))
         {
-
+            transform.rotation = Quaternion.Euler(new Vector3(x_rotation, transform.eulerAngles.y, transform.eulerAngles.z));
+            x_rotation -= speed;
+            Debug.Log("下キーが押された");
         }
     }
 }
